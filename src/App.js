405ec50801ca6532b9bf63a939626a9343dea2a3
tranr5 <tranr4@gmail.com>
2022-11-22 17:32:02 -0500

    added css and started project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="left">
+            <div>
+              <p>Bill</p>
+              <input type="text" placeholder={'0.00'}/>
+            </div>
+            <div>
+              <div>Tips %</div>
+              <div>
+                <button type="button">5%</button>
+                <button type="button">10%</button>
+                <button type="button">15%</button>
+                <br></br><button type="button">20%</button>
+                <button type="button">25%</button>
+                <button type="button">Custom</button>
+              </div>
+            </div>
+            <div>
+              <div>
+                Number of People
+              </div>
+              <div>
+              <input type="text" placeholder={'0'}/>
+              </div>
+            </div>
+          </div>
+
+      <div className="right">
+        <div>
+          <p>Tip Amount</p>
+          <p>/ person</p>
+        <input type="text" placeholder={'0.00'}/>
+        </div>
+        <div>
+          <p>Total</p>
+          <p>/ person</p>
+          <input type="text" placeholder={'0.00'}/>
+        </div>
+      </div>
     </div>
   );
 }
