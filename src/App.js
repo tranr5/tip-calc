7a74f9708b5541328571c28792968a11ee32e0cb
tranr5 <tranr4@gmail.com>
2022-11-26 18:22:27 -0500

    moved to components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
 import './App.css';
+import SetForm from './components/SetForm';
 
 function App() {
   return (
-    <div className="App">
-          <div className="left">
-            <div>
-              <p>Bill</p>
-              <input type="text" placeholder={'0.00'}/>
-            </div>
-            <div>
-              <div>Tips %</div>
-              <div>
-                <button type="button">5%</button>
-                <button type="button">10%</button>
-                <button type="button">15%</button>
-                <br></br><button type="button">20%</button>
-                <button type="button">25%</button>
-                <button type="button">Custom</button>
-              </div>
-            </div>
-            <div>
-              <div>
-                Number of People
-              </div>
-              <div>
-              <input type="text" placeholder={'0'}/>
-              </div>
-            </div>
-          </div>
-
-      <div className="right">
-        <div>
-          <p>Tip Amount</p>
-          <p>/ person</p>
-        <input type="text" placeholder={'0.00'}/>
-        </div>
-        <div>
-          <p>Total</p>
-          <p>/ person</p>
-          <input type="text" placeholder={'0.00'}/>
-        </div>
-      </div>
-    </div>
+    <SetForm />
   );
 }
 
---
+++ src/components/SetForm.js
@@ -0,0 +1,50 @@
+import {useState, useEffect} from 'react'
+
+const SetForm = () => {
+
+  
+    return (
+      <div className="App">
+          <div className="left">
+            <div>
+              <p>Bill</p>
+              <input type="text" placeholder={'0.00'}/>
+            </div>
+            <div>
+              <div>Tips %</div>
+              <div>
+                <button type="button">5%</button>
+                <button type="button">10%</button>
+                <button type="button">15%</button>
+                <br></br><button type="button">20%</button>
+                <button type="button">25%</button>
+                <button type="button">Custom</button>
+              </div>
+            </div>
+            <div>
+              <div>
+                Number of People
+              </div>
+              <div>
+              <input type="text" placeholder={'0'}/>
+              </div>
+            </div>
+          </div>
+
+      <div className="right">
+        <div>
+          <p>Tip Amount</p>
+          <p>/ person</p>
+        <input type="text" placeholder={'0.00'}/>
+        </div>
+        <div>
+          <p>Total</p>
+          <p>/ person</p>
+          <input type="text" placeholder={'0.00'}/>
+        </div>
+      </div>
+    </div>
+    )
+}
+
+export default SetForm
