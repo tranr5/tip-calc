25b56bc69d6169c25cd1aa49d80dc44be51e7619
tranr5 <tranr4@gmail.com>
2022-11-27 16:00:04 -0500

    added useStates

--- src/components/SetForm.js
+++ src/components/SetForm.js
@@ -2,23 +2,42 @@ import {useState, useEffect} from 'react'
 
 const SetForm = () => {
 
-  
+    const [amount, setAmount]  = useState(0)
+    const [tip, setTip] = useState(0)
+    const [total, setTotal] = useState(0)
+    const [tipTotal, setTipTotal] = useState(0)
+    const perFunc = (e)=>{
+      if (e) {
+          e.preventDefault() 
+      }
+    
+  }
+
+  useEffect(() => {
+    setTipTotal(tip*amount)
+    setTotal(tipTotal)
+  },[amount,tip])
+
+  const resetFunc = ()=>setAmount(0)
+
     return (
+      <form>
       <div className="App">
           <div className="left">
             <div>
               <p>Bill</p>
-              <input type="text" placeholder={'0.00'}/>
+              <input className="input" placeholder='0.00' onChange = {(e)=> setAmount(e.target.value)}/>
             </div>
             <div>
               <div>Tips %</div>
               <div>
-                <button type="button">5%</button>
+                <button type="button" value={0.05} onClick={perFunc}>5%</button>
                 <button type="button">10%</button>
                 <button type="button">15%</button>
                 <br></br><button type="button">20%</button>
                 <button type="button">25%</button>
                 <button type="button">Custom</button>
+                onChange = {(e)=>setTip(e.target.value/100)}
               </div>
             </div>
             <div>
@@ -40,10 +59,14 @@ const SetForm = () => {
         <div>
           <p>Total</p>
           <p>/ person</p>
-          <input type="text" placeholder={'0.00'}/>
+          <div className='totals' input type="text" placeholder={'0.00'}/>${tipTotal}
         </div>
+        <div className='button-reset' >
+                            <button className='button-reset_fill' onClick={resetFunc} > RESET </button>  
+                            </div>
       </div>
     </div>
+    </form>
     )
 }
 
